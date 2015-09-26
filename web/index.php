47e479f584cb6139d1a1d14ad10949714cc5dff0
Aleksander Piekarz <aleksander.piekarz@smtsoftware.com>
2015-09-26 13:57:56 +0200

    added imports

--- web/index.php
+++ web/index.php
@@ -1,14 +1,17 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->put('/user/register', function ($name) use ($app) {
+$app->put('/user/register', function (Request $request) use ($app) {
     return JsonResponse('Hello world, this is register');
 });
 
-$app->post('/user/login', function ($name) use ($app) {
+$app->post('/user/login', function (Request $request) use ($app) {
     return JsonResponse('Hello world, this is login');
 });
 $app['debug'] = true;
