32d5c75e1fab7450368f8bca9f388b83c8616012
Aleksander Piekarz <aleksander.piekarz@smtsoftware.com>
2015-09-26 12:18:34 +0200

    added stubs for register and login

--- web/index.php
+++ web/index.php
@@ -5,11 +5,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->put('/user/register', function ($name) use ($app) {
-    return true;
+    return JsonResponse('Hello world, this is register');
 });
 
 $app->post('/user/login', function ($name) use ($app) {
-    return true;
+    return JsonResponse('Hello world, this is login');
 });
 
 $app->run();
